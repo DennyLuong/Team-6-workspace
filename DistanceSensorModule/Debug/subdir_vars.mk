9de0b169a8e04da05607f62b76fccfeb6ad694dc
rduenez01 <duenez.robert@gmail.com>
2017-03-10 04:25:23 -0600

    Addition of distance sensor
    Utilizes AIN1 located at PE2.

---
+++ DistanceSensorModule/Debug/subdir_vars.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+LIB_SRCS += \
+C:/ti/tirtos_tivac_2_16_00_08/products/TivaWare_C_Series-2.1.1.71b/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../main.c \
+../tm4c123gh6pm_startup_ccs.c \
+C:/ti/tirtos_tivac_2_16_00_08/products/TivaWare_C_Series-2.1.1.71b/utils/uartstdio.c 
+
+OBJS += \
+./main.obj \
+./tm4c123gh6pm_startup_ccs.obj \
+./uartstdio.obj 
+
+C_DEPS += \
+./main.d \
+./tm4c123gh6pm_startup_ccs.d \
+./uartstdio.d 
+
+C_DEPS__QUOTED += \
+"main.d" \
+"tm4c123gh6pm_startup_ccs.d" \
+"uartstdio.d" 
+
+OBJS__QUOTED += \
+"main.obj" \
+"tm4c123gh6pm_startup_ccs.obj" \
+"uartstdio.obj" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../tm4c123gh6pm_startup_ccs.c" \
+"C:/ti/tirtos_tivac_2_16_00_08/products/TivaWare_C_Series-2.1.1.71b/utils/uartstdio.c" 
+
+
---
+++ DistanceSensorModule/main.c
@@ -0,0 +1,98 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/adc.h"
+#include "utils/uartstdio.h"
+
+void UARTinit(void);
+void ADCinit(void);
+
+int main(void) {
+	uint32_t ui32ADC0Value[4];
+	volatile uint32_t ui32DistAvg;
+
+	// Sets clock
+	SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+	UARTinit();		//UART Initialize
+	ADCinit();		//ADC Initialize
+	
+
+
+// UARTStdioConfig(UART0_BASE, 115200, SysCtlClockGet());        TRYING TO SET UP Standard I/O to use functions
+//    UARTprintf("Enter text: ");
+    while (1)
+    {
+    	ADCIntClear(ADC0_BASE, 1);
+    	ADCProcessorTrigger(ADC0_BASE, 1);
+    	UARTStdioConfig(UART0_BASE, 115200, SysCtlClockGet());
+
+    	while(!ADCIntStatus(ADC0_BASE, 1, false))
+    	{
+    	}
+    	ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
+    	ui32DistAvg = (ui32ADC0Value[0] + ui32ADC0Value[1] + ui32ADC0Value[2] + ui32ADC0Value[3] + 2)/4;
+
+
+
+    	// FIX: output avg distance to terminal
+    	// FIX: fix range value for inches or centimeters (OPTIONAL)
+        UARTCharPut(UART0_BASE, 'D');
+        UARTCharPut(UART0_BASE, 'i');
+        UARTCharPut(UART0_BASE, 's');
+        UARTCharPut(UART0_BASE, 't');
+        UARTCharPut(UART0_BASE, 'a');
+        UARTCharPut(UART0_BASE, 'n');
+        UARTCharPut(UART0_BASE, 'c');
+        UARTCharPut(UART0_BASE, 'e');
+        UARTCharPut(UART0_BASE, ':');
+        UARTCharPut(UART0_BASE, ' ');
+
+/*
+    	UARTCharPut(UART0_BASE, ui32DistAvg));
+    	UARTprintf(ui32DistAvg);
+*/
+
+
+    }
+
+    return 0;
+
+
+}
+
+
+
+
+void UARTinit(void) {
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+	GPIOPinConfigure(GPIO_PA0_U0RX);
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+	   (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+}
+
+void ADCinit(void) {
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+	GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);
+
+	ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
+		ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_CH0  );
+		ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CTL_CH1  );
+		ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CTL_CH2  );
+		ADCSequenceStepConfigure(ADC0_BASE, 1, 3,ADC_CTL_CH3  |ADC_CTL_IE|ADC_CTL_END);
+		ADCSequenceEnable(ADC0_BASE, 1);
+
+}
+
