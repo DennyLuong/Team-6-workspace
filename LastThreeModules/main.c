ed009eb9dcd52de0c33bc30754cadc80ca917ed3
Sikender Shahid <sikendershahid91@gmail.com>
2017-03-15 22:28:05 -0500

    changing UART0 to UART1

--- LastThreeModules/main.c
+++ LastThreeModules/main.c
@@ -14,19 +14,19 @@
 #include "CommandInterpreter.h"
 
 void UArtInit(void){
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    GPIOPinConfigure(GPIO_PB0_U1RX);
+    GPIOPinConfigure(GPIO_PB1_U1TX);
+    GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+    UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 115200,
        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
     
     IntMasterEnable(); //enable processor interrupts
-    IntEnable(INT_UART0); //enable the UART interrupt
-    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    IntEnable(INT_UART1); //enable the UART interrupt
+    UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
 }
 
 void ADCInit(void){
@@ -44,29 +44,29 @@ void ADCInit(void){
 }
 
 void UARTIntHandler(void){
-	volatile uint32_t status = UARTInitStatus(UART0_BASE, true);  
+	volatile uint32_t status = UARTInitStatus(UART1_BASE, true);  
 	volatile uint32_t character; 
 	uint32_t * characters[2]; 
 
-	UARTInitClear(UART0_BASE)
+	UARTInitClear(UART1_BASE);
 }
 
 int main(void) {
 	UArtInit(); 
 	ADCInit(); 
 
-	UARTCharPut(UART0_BASE, 'E');
-    UARTCharPut(UART0_BASE, 'n');
-    UARTCharPut(UART0_BASE, 't');
-    UARTCharPut(UART0_BASE, 'e');
-    UARTCharPut(UART0_BASE, 'r');
-    UARTCharPut(UART0_BASE, ' ');
-    UARTCharPut(UART0_BASE, 'T');
-    UARTCharPut(UART0_BASE, 'e');
-    UARTCharPut(UART0_BASE, 'x');
-    UARTCharPut(UART0_BASE, 't');
-    UARTCharPut(UART0_BASE, ':');
-    UARTCharPut(UART0_BASE, ' ');
+    UARTCharPut(UART1_BASE, 'E');
+    UARTCharPut(UART1_BASE, 'n');
+    UARTCharPut(UART1_BASE, 't');
+    UARTCharPut(UART1_BASE, 'e');
+    UARTCharPut(UART1_BASE, 'r');
+    UARTCharPut(UART1_BASE, ' ');
+    UARTCharPut(UART1_BASE, 'T');
+    UARTCharPut(UART1_BASE, 'e');
+    UARTCharPut(UART1_BASE, 'x');
+    UARTCharPut(UART1_BASE, 't');
+    UARTCharPut(UART1_BASE, ':');
+    UARTCharPut(UART1_BASE, ' ');
 
     while (1)
         ;
