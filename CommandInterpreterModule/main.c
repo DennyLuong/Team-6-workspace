4b0ae2fd7d0eb8b362099edeb382f95b8aa4b1ff
Sikender Shahid <sikendershahid91@gmail.com>
2017-03-21 17:54:07 -0500

    deleted uartstdio, no need for that:

--- CommandInterpreterModule/main.c
+++ CommandInterpreterModule/main.c
@@ -13,7 +13,6 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 #include "driverlib/adc.h"
-#include "utils/uartstdio.h"
 
 #define UART_BUFFERED 1
 
@@ -200,6 +199,12 @@ void frontDistance(void){
     delay();
 }
 
+void print(const char * input, uint32_t len){
+    uint32_t count;
+    for(count =0 ; count< len; count++){
+        UARTCharPut(UART0_BASE, input[count]);
+    }
+}
 
 void ADCinit(void) {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
@@ -225,7 +230,7 @@ void delay(void)
 int main(void) {
 
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
- //   SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
     GPIOPinConfigure(GPIO_PA0_U0RX);
@@ -235,13 +240,17 @@ int main(void) {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //enable GPIO port for LED
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2); //enable pin for LED PF2
 
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+           (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 
     ADCinit();
     IntMasterEnable(); //enable processor interrupts
     IntEnable(INT_UART0); //enable the UART interrupt
     UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT); //only enable RX and TX interrupts
-    UARTStdioConfig(0,115200, SysCtlClockGet());
-    UARTprintf("Enter Command\n");
+    //UARTStdioConfig(0,115200, SysCtlClockGet());
+   // UARTprintf("Enter Command\n");
+
+    print("12345", 5);
 
     while (1)
         ;
