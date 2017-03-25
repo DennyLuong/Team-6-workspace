29b7610f7ce51001d09b18754d48e876bfa92885
rduenez01 <duenez.robert@gmail.com>
2017-03-24 19:13:41 -0500

    Additon of UARTstdio
    printf is now available, removal of print( ); added strcmp(); Addition
    of comments

--- Command_Interpreter/main.c
+++ Command_Interpreter/main.c
@@ -11,239 +11,248 @@
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 #include "driverlib/adc.h"
 #include "driverlib/pwm.h"
+#include "utils/uartstdio.h"
 #include "driverlib/debug.h"
+#include <string.h>
 
 #define PWM_FREQUENCY 55
 
 typedef enum{
-    HH=0,
-    P1=2,
-    P0=4,
-    MF=6,
-    RR=8,
-    SS=10,
-    LR=12,
-    LG=14,
-    GO=16,
-    R0=18,
-    R1=20,
-    TD=22,
-    DS=24,
-    ES=26,
-    DC=28,
-    ER=30
+	HH=0,
+	P1=2,
+	P0=4,
+	MF=6,
+	RR=8,
+	SS=10,
+	LR=12,
+	LG=14,
+	GO=16,
+	R0=18,
+	R1=20,
+	TD=22,
+	DS=24,
+	ES=26,
+	DC=28,
+	ER=30
 }Commands;
 
 
 
 uint8_t CharacterCount = 0;
 
-char responseGet(uint32_t chars[]){
-    static const char * LookupTable[] = {
-            "HH", " Enter 2char function      ",
-            "P1", " PWM enable                ",
-            "P0", " PWM disable               ",
-            "MF", " Moving Forward            ",
-            "RR", " Right Wheel Reverse       ",
-            "SS", " Set Speed                 ",
-            "LR", " Left Wheel Reverse        ",
-            "LG", " Light Get                 ",
-            "GO", " PID initiated             ",
-            "R0", " Read Front Distance Sensor",
-            "R1", " Read Right Distance Sensor",
-            "TD", " Toggle Data Acquisition   ",
-            "DS", " Post Drive Semaphore      ",
-            "ES", " Emergency Stop            ",
-            "DC", " Drive Clock Start         ",
-            "ER", " Error                     "
-        };
+char responseGet(char chars[]){
+	static const char * LookupTable[] = {
+			"HH", " Enter 2char function      ",
+			"P1", " PWM enable                ",
+			"P0", " PWM disable               ",
+			"MF", " Moving Forward            ",
+			"RR", " Right Wheel Reverse       ",
+			"SS", " Set Speed                 ",
+			"LR", " Left Wheel Reverse        ",
+			"LG", " Light Get                 ",
+			"GO", " PID initiated             ",
+			"R0", " Read Front Distance Sensor",
+			"R1", " Read Right Distance Sensor",
+			"TD", " Toggle Data Acquisition   ",
+			"DS", " Post Drive Semaphore      ",
+			"ES", " Emergency Stop            ",
+			"DC", " Drive Clock Start         ",
+			"ER", " Error                     "
+	};
 
-    char index = 0;
-    if(chars[0] == 'H' && chars[1] == 'H')
-        index = HH;
-    else if(chars[0] == 'P' && chars[1] == '1')
-        index = P1;
-    else if(chars[0] == 'P' && chars[1] == '0')
-            index = P0;
-    else if(chars[0] == 'M' && chars[1] == 'F')
-            index = MF;
-    else if(chars[0] == 'R' && chars[1] == 'R')
-            index = RR;
-    else if(chars[0] == 'S' && chars[1] == 'S')
-            index = SS;
-    else if(chars[0] == 'L' && chars[1] == 'R')
-            index = LR;
-    else if(chars[0] == 'G' && chars[1] == 'O')
-            index = GO;
-    else if(chars[0] == 'R' && chars[1] == '0')
-            index = R0;
-    else if(chars[0] == 'R' && chars[1] == '1')
-            index = R1;
-    else if(chars[0] == 'T' && chars[1] == 'D')
-            index = TD;
-    else if(chars[0] == 'D' && chars[1] == 'S')
-            index = DS;
-    else if(chars[0] == 'E' && chars[1] == 'S')
-            index = ES;
-    else if(chars[0] == 'D' && chars[1] == 'C')
-            index = DC;
-    else
-        index = ER;
+	char index = 0;
+	if(!strcmp(chars,"HH"))
+		index = HH;
+	else if(!strcmp(chars,"P1"))
+		index = P1;
+	else if(!strcmp(chars,"P0"))
+		index = P0;
+	else if(!strcmp(chars,"MF"))
+		index = MF;
+	else if(!strcmp(chars,"RR"))
+		index = RR;
+	else if(!strcmp(chars,"SS"))
+		index = SS;
+	else if(!strcmp(chars,"LR"))
+		index = LR;
+	else if(!strcmp(chars,"GO"))
+		index = GO;
+	else if(!strcmp(chars,"R0"))
+		index = R0;
+	else if(!strcmp(chars,"R1"))
+		index = R1;
+	else if(!strcmp(chars,"TD"))
+		index = TD;
+	else if(!strcmp(chars,"DS"))
+		index = DS;
+	else if(!strcmp(chars,"ES"))
+		index = ES;
+	else if(!strcmp(chars,"DC"))
+		index = DC;
+	else
+		index = ER;
 
-    UARTCharPut(UART1_BASE, 0x3A);
-    UARTCharPut(UART1_BASE, 0x20);
-    print(LookupTable[index], 2);
-    UARTCharPut(UART1_BASE, 0x20);
-    print(LookupTable[index+1], 27);
-    UARTCharPut(UART1_BASE, 0x20);
-    UARTCharPut(UART1_BASE, 0x0D);
-    UARTCharPut(UART1_BASE, 0x0A);
-    return index;
+	//UARTCharPut(UART1_BASE, 0x3A); // :
+	//UARTCharPut(UART1_BASE, 0x20); // space
+	UARTprintf(": ");
+	UARTprintf(LookupTable[index]);
+
+	UARTCharPut(UART1_BASE, 0x20); 	// space
+
+	UARTprintf(LookupTable[index + 1]);
+	UARTCharPut(UART1_BASE, 0x20);	// space
+	UARTCharPut(UART1_BASE, 0x0D);	// ??
+	UARTCharPut(UART1_BASE, 0x0A);	// ??
+
+	return index;
 }
 
-
-
-void UARTIntHandler(void)
-{
-     uint32_t ui32Status;
-     uint32_t character;
-     uint32_t characters[2];
-     char function=0;
-
-     ui32Status = UARTIntStatus(UART1_BASE, true); //get interrupt status
-     UARTIntClear(UART1_BASE, ui32Status); //clear the asserted interrupts
-
-     while(UARTCharsAvail(UART1_BASE)) //loop while there are chars
-     {
-         character = UARTCharGetNonBlocking(UART1_BASE);
-         UARTCharPutNonBlocking(UART1_BASE, character); //echo character
-         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //blink LED
-          //turn off LED
-         characters[CharacterCount] = character;
-         CharacterCount++;
-         if(CharacterCount == 2){
-             CharacterCount =0;
-             UARTCharPut(UART1_BASE, '\r');
-             UARTCharPut(UART1_BASE, '\n');
-             function = responseGet(characters);
-             getFunction(function);
-             print("Enter Command:", 14);
-
-         }
-
-         SysCtlDelay(SysCtlClockGet() / (1000 * 3)); //delay ~1 msec
-         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-    }
-}
-
-void getFunction(char function){
-    switch (function) {
-    case R0:
-        frontDistance();
-        break;
-    case MF:
-    	PWMForward();
-    	break;
-    case ES:
-    	PWMStop();
-    case SS:
-    	PWMSpeed();
-    default:
-		print("error",5);
-    }
-
-
-}
 
 void frontDistance(void){
-    uint32_t ui32ADC0Value[4];
-    volatile uint32_t ui32DistAvg;
-    volatile uint32_t ui32temp;
-    uint8_t ui8outChar[5];
-    int i = 0;
-    int decimalNum = 0;
+	uint32_t ui32ADC0Value[4];
+	volatile uint32_t ui32DistAvg;
 
-    ADCIntClear(ADC0_BASE, 1);
-    ADCProcessorTrigger(ADC0_BASE, 1);
-    //UARTStdioConfig(UART1_BASE, 115200, SysCtlClockGet());
+	// Clear interrupt
+	ADCIntClear(ADC0_BASE, 1);
+	ADCProcessorTrigger(ADC0_BASE, 1);	// Trigger ADC Conversion
+	// Wait on conversion to finish
+	while(!ADCIntStatus(ADC0_BASE, 1, false))
+	{
+	}
+	ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
+	ui32DistAvg = (ui32ADC0Value[0] + ui32ADC0Value[1] + ui32ADC0Value[2] + ui32ADC0Value[3] + 2)/4;
 
-    while(!ADCIntStatus(ADC0_BASE, 1, false))
-    {
-    }
-    ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
-    ui32DistAvg = (ui32ADC0Value[0] + ui32ADC0Value[1] + ui32ADC0Value[2] + ui32ADC0Value[3] + 2)/4;
+	// FIX: output avg distance to terminal
+	// FIX: fix range value for inches or centimeters (OPTIONAL)
 
+	UARTprintf("Distance: ");
+	UARTprintf("%d \n",ui32DistAvg);
 
-
-    // FIX: output avg distance to terminal
-    // FIX: fix range value for inches or centimeters (OPTIONAL)
-    print("Distance: ");
-
-    ui32temp = ui32DistAvg;
-    decimalNum = 0;
-    for (i = 0; ui32temp >= 1; i++) {
-        ui32temp = ui32temp/10;
-        decimalNum++;
-    }
-    ui8outChar[0] = 'a';
-    ui8outChar[1] = ' ';
-    ui8outChar[2] = ' ';
-    ui8outChar[3] = ' ';
-    ui8outChar[4] = ' ';
-
-    for (i = decimalNum - 1; i >= 0; i--) {
-        ui8outChar[i] = (char)48 + (ui32DistAvg % 10);
-        ui32DistAvg /= 10;
-    }
-    for (i = 0; i < decimalNum; i++) {
-        UARTCharPut(UART1_BASE, ui8outChar[i]);
-    }
-
-    UARTCharPut(UART1_BASE, '\r');
-    UARTCharPut(UART1_BASE, '\n');
-    SysCtlDelay(6700000);
-}
-
-void print(const char * input, uint32_t len){
-    uint32_t count;
-    for(count =0 ; count< len; count++){
-        UARTCharPut(UART1_BASE, input[count]);
-    }
-}
-
-void UARTInit(void){
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-}
-
-void ADCInit(void) {
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
-
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);
-
-    ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
-    ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_CH0  );
-    ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CTL_CH1  );
-    ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CTL_CH2  );
-    ADCSequenceStepConfigure(ADC0_BASE, 1, 3,ADC_CTL_CH3  |ADC_CTL_IE|ADC_CTL_END);
-    ADCSequenceEnable(ADC0_BASE, 1);
+	SysCtlDelay(6700000);
 }
 
 
 unsigned int ui32Load = 0;
 unsigned int ui32PWMClock = 0;
 unsigned int ui8Adjust = 83;
+
+void PWMForward(void){
+	// forward
+	ui8Adjust = ui32Load;
+	GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_3, 12);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, ui8Adjust);
+}
+
+void PWMStop(void){
+	PWMOutputState(PWM1_BASE, (PWM_OUT_0_BIT|PWM_OUT_1_BIT), false);
+}
+
+
+
+void PWMSpeed(void){
+
+	char inSpeed[4];
+	int speedAdjust;
+
+	UARTprintf("Enter Speed(1-100):");
+
+	UARTgets(inSpeed,strlen(inSpeed) + 1);
+
+	speedAdjust = atoi(inSpeed);		//conversion from ASCII to INT
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, speedAdjust * ui32Load/100);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, speedAdjust * ui32Load/100);
+}
+
+void getFunction(char function){
+	// MAIN COMMAND SELECTOR
+	switch (function) {
+	case R0:
+		frontDistance();
+		break;
+	case MF:
+		PWMForward();
+		break;
+	case ES:
+		PWMStop();
+	case SS:
+		PWMSpeed();
+	default:
+		UARTprintf("Error \n");
+	}
+
+}
+
+/*
+void UARTIntHandler(void)
+{
+
+
+	while(UARTCharsAvail(UART1_BASE)) //loop while there are chars
+	{
+		character = UARTCharGetNonBlocking(UART1_BASE);
+		UARTCharPutNonBlocking(UART1_BASE, character); //echo character
+		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //blink LED
+		//turn off LED
+		characters[CharacterCount] = character;
+		CharacterCount++;
+		if(CharacterCount == 2){
+			CharacterCount = 0;
+			UARTCharPut(UART1_BASE, '\r');
+			UARTCharPut(UART1_BASE, '\n');
+			function = responseGet(characters);
+			getFunction(function);
+			//print("Enter Command:", 14);
+			UARTprintf("Enter Command: ");
+
+		}
+			SysCtlDelay(SysCtlClockGet() / (1000 * 3)); //delay ~1 msec
+	}
+
+
+
+}
+
+*/
+
+
+
+void UARTInit(void){
+	// Enable UART & GPIO peripherals
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	// Configuration of GPIO pins and types
+	GPIOPinConfigure(GPIO_PB0_U1RX);
+	GPIOPinConfigure(GPIO_PB1_U1TX);
+	GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	// set clock and standard I/O
+	UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
+	UARTStdioConfig(1, 115200, 16000000);
+	UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
+
+	//UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+}
+
+void ADCInit(void) {
+	// Enable ADC and GPIO E PIN 2 for ADC
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+	GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);
+	// Set sequence and configure steps
+	ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_CH0  );
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CTL_CH1  );
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CTL_CH2  );
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 3,ADC_CTL_CH3  |ADC_CTL_IE|ADC_CTL_END);
+	ADCSequenceEnable(ADC0_BASE, 1);
+}
+
 
 
 void PWMInit(void)
@@ -262,6 +271,7 @@ void PWMInit(void)
 	GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE,GPIO_PIN_1);	// MODE
 	GPIOPinConfigure(GPIO_PD0_M1PWM0);
 	GPIOPinConfigure(GPIO_PD1_M1PWM1);
+
 	ui32PWMClock = SysCtlClockGet() / 64;
 	ui32Load = (ui32PWMClock / PWM_FREQUENCY) - 1;
 	PWMGenConfigure(PWM1_BASE, PWM_GEN_0, PWM_GEN_MODE_DOWN);
@@ -276,45 +286,43 @@ void PWMInit(void)
 	PWMGenEnable(PWM1_BASE, PWM_GEN_0);
 }
 
-void PWMForward(void){
-	// forward
-	ui8Adjust = ui32Load;
-	GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_3, 12);
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust);
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, ui8Adjust);
-}
-
-void PWMStop(void){
-	PWMOutputState(PWM1_BASE, (PWM_OUT_0_BIT|PWM_OUT_1_BIT), false);
-}
-
-void PWMSpeed(void){
-	print("Enter Speed (0-9",3);
-	ui32Status = UARTIntStatus(UART1_BASE, true); //get interrupt status
-	     UARTIntClear(UART1_BASE, ui32Status); //clear the asserted interrupts
-
-	     while(UARTCharsAvail(UART1_BASE)) //loop while there are chars
-	     {
-	         character = UARTCharGetNonBlocking(UART1_BASE);
-	         UARTCharPutNonBlocking(UART1_BASE, character);
-
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, speed * ui8Adjust/100);
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, speed * ui8Adjust/100);
-}
-
 
 int main(void) {
-    UARTInit();
-    ADCInit();
-    PWMInit();
 
-    IntMasterEnable();
-    IntEnable(INT_UART1);
-    UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
+	UARTInit();
+	ADCInit();
+	PWMInit();
 
-    print("Embedded Systems - TEAM 6 \r\n",29);
-    print("Enter Command:", 14);
+	IntMasterEnable();
+	IntEnable(INT_UART1);
 
-    while (1)
-        ;
+	UARTprintf("Embedded Systems - TEAM 6 \r\n");
+	UARTprintf("Enter Command:");
+	//print("Embedded Systems - TEAM 6 \r\n",29);
+	//print("Enter Command:", 14);
+
+	while (1)
+	{
+		uint32_t ui32Status;
+		//uint32_t character;
+		char characters[3];
+		char function = 0;
+
+
+		UARTgets(characters, strlen(characters) + 1);
+		UARTprintf(characters);
+		UARTprintf("\n");
+
+		ui32Status = UARTIntStatus(UART1_BASE, true); //get interrupt status
+
+		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //blink LED
+		function = responseGet(characters);
+		getFunction(function);
+		UARTprintf("Enter Command: ");
+
+
+		SysCtlDelay(SysCtlClockGet() / (1000 * 3)); //delay ~1 msec
+		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+		UARTIntClear(UART1_BASE, ui32Status); //clear the asserted interrupts
+	}
 }
