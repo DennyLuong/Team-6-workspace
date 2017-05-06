e13d3faef651665592d6aec0de66b0b8f242dbd3
rduenez01 <duenez.robert@gmail.com>
2017-05-05 19:05:57 -0500

    Addition of buffSem
    prints the buffer

--- RTOS_Milestones/main.c
+++ RTOS_Milestones/main.c
@@ -43,22 +43,22 @@
  *                                     ADC get_adc_value Functions                                      *
  ********************************************************************************************************/
 uint32_t rightDistance(void){
-    //ADC_CTL_CH0
+	//ADC_CTL_CH0
 	uint32_t ui32ADC0Value[4];
 	ADCIntClear(ADC0_BASE, 1);
 	ADCProcessorTrigger(ADC0_BASE, 1);
 	while(!ADCIntStatus(ADC0_BASE, 1, false)){}
 	ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
-    return ui32ADC0Value[0];
+	return ui32ADC0Value[0];
 }
 uint32_t frontDistance(void){
-    //ADC_CTL_CH1
+	//ADC_CTL_CH1
 	uint32_t ui32ADC0Value[4];
 	ADCIntClear(ADC0_BASE, 2);
 	ADCProcessorTrigger(ADC0_BASE, 2);
 	while(!ADCIntStatus(ADC0_BASE, 2, false)){}
 	ADCSequenceDataGet(ADC0_BASE, 2, ui32ADC0Value);
-    return ui32ADC0Value[0];
+	return ui32ADC0Value[0];
 }
 /********************************************************************************************************
  *                                             PWM Functions                                            *
@@ -74,15 +74,15 @@ void move_forward(void){
 }
 
 void move_reverse(void){
-    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_7, 0|0);
+	GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_7, 0|0);
 }
 
 void move_cw(void){
-    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_7, GPIO_PIN_2|0);
+	GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_7, GPIO_PIN_2|0);
 }
 
 void move_ccw(void){
-    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_7, GPIO_PIN_7|0);
+	GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2|GPIO_PIN_7, GPIO_PIN_7|0);
 }
 
 void PWMStop(void){
@@ -90,13 +90,13 @@ void PWMStop(void){
 }
 
 void PWMSetSpeed(unsigned int left_adjust, unsigned int right_adjust){
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, left_adjust * ui32Load/100);
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, right_adjust * ui32Load/100);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, left_adjust * ui32Load/100);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, right_adjust * ui32Load/100);
 }
 
 void PWMStart(void){
-    PWMOutputState(PWM1_BASE, (PWM_OUT_0_BIT|PWM_OUT_1_BIT), true);
-    PWMSetSpeed(ui32Load, ui32Load);
+	PWMOutputState(PWM1_BASE, (PWM_OUT_0_BIT|PWM_OUT_1_BIT), true);
+	PWMSetSpeed(ui32Load, ui32Load);
 }
 
 void PWMUserSetSpeed(void){
@@ -108,28 +108,43 @@ void PWMUserSetSpeed(void){
 /********************************************************************************************************
  *                                          Double Buffer                                               *
  ********************************************************************************************************/
-void swap(char * buffer1 , char * buffer2 ){
-    char * temp = buffer1;
-    buffer1 = buffer2;
-    buffer2 = temp;
-}
 
 char distanceBuffer[20];
 char distanceBuffer2[20];
 int distanceBufferCount = 0;
+
+void swap(char * buffer1 , char * buffer2 ){
+	char * temp = buffer1;
+	buffer1 = buffer2;
+	buffer2 = temp;
+}
+
+
+void printBufferPing() {
+	while(1) {
+		Semaphore_pend(buffSem, BIOS_WAIT_FOREVER);
+
+		for(distanceBufferCount = 0 ; distanceBufferCount <20 ; distanceBufferCount++){
+			UARTprintf("%d", distanceBuffer[distanceBufferCount]);
+			UARTprintf("\n");
+		}
+		swap(distanceBuffer, distanceBuffer2);
+		distanceBufferCount = 0;
+	}
+}
+
+
+bool bufferFull = false;
+
 void distanceBufferLog(int ErrorValue){
-    if(distanceBufferCount != 20){
-        distanceBuffer[distanceBufferCount] = ErrorValue;
-        distanceBufferCount++;
-    }
-    else{
-        for(distanceBufferCount = 0 ; distanceBufferCount <20 ; distanceBufferCount++){
-            UARTprintf("%d", distanceBuffer[distanceBufferCount]);
-            UARTprintf("\n");
-        }
-        swap(distanceBuffer, distanceBuffer2);
-        distanceBufferCount = 0;
-    }
+	if(distanceBufferCount != 20){
+		distanceBuffer[distanceBufferCount] = ErrorValue;
+		distanceBufferCount++;
+	}
+	else{
+		UARTprintf("post semaphore \n");
+		Semaphore_post(buffSem);
+	}
 }
 
 /********************************************************************************************************
@@ -139,57 +154,59 @@ uint32_t time = 0;
 uint32_t previousTimeRead = 0;
 uint32_t initialTime, initialLine, finalLine, thresholdBlackLine;
 int blackLineWidth = 0;
-void blackLineFound(bool blackLineToggle)
+bool lineDetectToggle = 0;
+void blackLineFound(void)
 {
-    TimerDisable(TIMER2_BASE, TIMER_A);
-    SysCtlDelay(100);
-    TimerEnable(TIMER0_BASE,TIMER_BOTH);
-    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_4 );
-    GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_4  , GPIO_PIN_4  );
-    SysCtlDelay(100);
-    GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_4  );
-    initialTime = TimerValueGet(TIMER0_BASE, TIMER_A);
-    while(GPIOPinRead(GPIO_PORTE_BASE,GPIO_PIN_4)){};
-    time = TimerValueGet(TIMER0_BASE, TIMER_A) - initialTime;
+	TimerDisable(TIMER2_BASE, TIMER_A);
+	SysCtlDelay(100);
+	TimerEnable(TIMER0_BASE,TIMER_BOTH);
+	GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_4 );
+	GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_4  , GPIO_PIN_4  );
+	SysCtlDelay(100);
+	GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_4  );
+	initialTime = TimerValueGet(TIMER0_BASE, TIMER_A);
+	while(GPIOPinRead(GPIO_PORTE_BASE,GPIO_PIN_4)){};
+	time = TimerValueGet(TIMER0_BASE, TIMER_A) - initialTime;
 
-    //white noise handle
-    if(time < 400){
-        time = 0;
-    }
-    //first detection of black line
-    if(time - previousTimeRead > 0 && initialLine == 0)
-        initialLine = time;
+	//white noise handle
+	if(time < 400){
+		time = 0;
+	}
+	//first detection of black line
+	if(time - previousTimeRead > 0 && initialLine == 0)
+		initialLine = time;
 
-    //detection of black line decreasing
-    if(time - previousTimeRead < 0)
-        finalLine = time;
+	//detection of black line decreasing
+	if(time - previousTimeRead < 0)
+		finalLine = time;
 
-    //thresholding value
-    thresholdBlackLine = finalLine - initialLine;
-    //threshold action
-    if(thresholdBlackLine!= 0){
-            blackLineWidth++;
-    }
-    if(thresholdBlackLine == 0){
-        blackLineWidth = 0;
-    }
-    if (blackLineWidth >5){
-        PWMStop();
-    }
-    if (blackLineWidth > 2){
-        blackLineToggle = !(blackLineToggle);
-    }
-    // clearing out values after black line detection
-    previousTimeRead = time;
-    if(previousTimeRead == 0 && time == 0){
-        initialLine = 0;
-        finalLine = 0;
-        thresholdBlackLine = 0;
-        blackLineWidth = 0;
-    }
-    TimerDisable(TIMER0_BASE, TIMER_BOTH);
-    SysCtlDelay(100);
-    TimerEnable(TIMER2_BASE, TIMER_A);
+	//thresholding value
+	thresholdBlackLine = finalLine - initialLine;
+	//threshold action
+	if(thresholdBlackLine!= 0){
+		blackLineWidth++;
+	}
+	if(thresholdBlackLine == 0){
+		blackLineWidth = 0;
+	}
+	if (blackLineWidth >5){
+		PWMStop();
+	}
+	if (blackLineWidth > 3){
+		UARTprintf("width>3\n");
+		lineDetectToggle = !(lineDetectToggle);
+	}
+	// clearing out values after black line detection
+	previousTimeRead = time;
+	if(previousTimeRead == 0 && time == 0){
+		initialLine = 0;
+		finalLine = 0;
+		thresholdBlackLine = 0;
+		blackLineWidth = 0;
+	}
+	TimerDisable(TIMER0_BASE, TIMER_BOTH);
+	SysCtlDelay(100);
+	TimerEnable(TIMER2_BASE, TIMER_A);
 }
 
 void PIDStart(void) {
@@ -214,13 +231,13 @@ const float d_const = .0075;
 const int max = 100;
 int dataCollectionToggle = 0;
 int power_difference = 0;
-bool lineDetectToggle= 0;
+
 
 int computePID(void){
-//	if(blackLineFound()){
-//		lineDetectToggle = !(lineDetectToggle);
-//	}
-	blackLineFound(&lineDetectToggle);
+	//	if(blackLineFound()){
+	//		lineDetectToggle = !(lineDetectToggle);
+	//	}
+	blackLineFound();
 	// PID calculations
 	cur_pos = rightDistance();
 	proportional = cur_pos - target_dist;
@@ -231,7 +248,7 @@ int computePID(void){
 	power_difference = proportional/p_const + derivative * d_const + integral/i_const;
 
 	if(lineDetectToggle && dataCollectionToggle){
-	    distanceBufferLog(power_difference);
+		distanceBufferLog(power_difference);
 	}
 	dataCollectionToggle = 1 - dataCollectionToggle; // or dataCollectionToggle = !dataCollectionToggle;
 
@@ -240,33 +257,33 @@ int computePID(void){
 	if (power_difference < -max){
 		power_difference = -max;}
 
-//	UARTprintf("Power Difference: %d\n",power_difference);
+	//	UARTprintf("Power Difference: %d\n",power_difference);
 
-	  if(power_difference < 0){
-		  PWMSetSpeed(max, 101 + power_difference);
-	  }
-	  else if(power_difference > 0){
-		  PWMSetSpeed(max + 1 -power_difference, max);
-	  }
-	  else{
-		  PWMSetSpeed(max, max);
-	  }
-	  //front_target = 2200
-	  if(frontDistance() > front_target){
-		  while (!(frontDistance() < 1500))
-	  			move_cw();
-		  move_forward();
-	  }
+	if(power_difference < 0){
+		PWMSetSpeed(max, 101 + power_difference);
+	}
+	else if(power_difference > 0){
+		PWMSetSpeed(max + 1 -power_difference, max);
+	}
+	else{
+		PWMSetSpeed(max, max);
+	}
+	//front_target = 2200
+	if(frontDistance() > front_target){
+		while (!(frontDistance() < 1500))
+			move_cw();
+		move_forward();
+	}
 
-	  if(rightDistance() < 800 )
-	  {
-		  while(!(rightDistance() > 2400))
-	  		{
-	  			move_forward();
-	  			PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Load);
-	  			PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, ui32Load/2);
-	  		}
-	  	}
+	if(rightDistance() < 800 )
+	{
+		while(!(rightDistance() > 2400))
+		{
+			move_forward();
+			PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Load);
+			PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1, ui32Load/2);
+		}
+	}
 	return power_difference;
 }
 
@@ -285,156 +302,156 @@ void Timer2IntHandler(void) {
 
 
 typedef enum{
-    HH=0,
-    MS=2,
-    P0=4,
-    MF=6,
-    RR=8,
-    SS=10,
-    LR=12,
-    LG=14,
-    GO=16,
-    R0=18,
-    R1=20,
-    TD=22,
-    DS=24,
-    ES=26,
-    DC=28,
-    ER=30,
-    BL=32
+	HH=0,
+	MS=2,
+	P0=4,
+	MF=6,
+	RR=8,
+	SS=10,
+	LR=12,
+	LG=14,
+	GO=16,
+	R0=18,
+	R1=20,
+	TD=22,
+	DS=24,
+	ES=26,
+	DC=28,
+	ER=30,
+	BL=32
 }Commands;
 
 uint8_t CharacterCount = 0;
 
 char responseGet(char chars[]){
-    static const char * LookupTable[] = {
-            "HH", " Enter 2char function      ",
-            "MS", " PWM enable                ",
-            "P0", " PWM disable               ",
-            "MF", " Moving Forward            ",
-            "RR", " Right Wheel Reverse       ",
-            "SS", " Set Speed                 ",
-            "LR", " Left Wheel Reverse        ",
-            "LG", " Light Get                 ",
-            "GO", " PID initiated             ",
-            "R0", " Read Front Distance Sensor",
-            "R1", " Read Right Distance Sensor",
-            "TD", " Toggle Data Acquisition   ",
-            "DS", " Post Drive Semaphore      ",
-            "ES", " Emergency Stop            ",
-            "DC", " Drive Clock Start         ",
-            "ER", " Error                     ",
-            "BL", " Black Line                ",
-    };
+	static const char * LookupTable[] = {
+			"HH", " Enter 2char function      ",
+			"MS", " PWM enable                ",
+			"P0", " PWM disable               ",
+			"MF", " Moving Forward            ",
+			"RR", " Right Wheel Reverse       ",
+			"SS", " Set Speed                 ",
+			"LR", " Left Wheel Reverse        ",
+			"LG", " Light Get                 ",
+			"GO", " PID initiated             ",
+			"R0", " Read Front Distance Sensor",
+			"R1", " Read Right Distance Sensor",
+			"TD", " Toggle Data Acquisition   ",
+			"DS", " Post Drive Semaphore      ",
+			"ES", " Emergency Stop            ",
+			"DC", " Drive Clock Start         ",
+			"ER", " Error                     ",
+			"BL", " Black Line                ",
+	};
 
-    char index = 0;
-    if(!strcmp(chars,"HH"))
-        index = HH;
-    else if(!strcmp(chars,"MS"))
-        index = MS;
-    else if(!strcmp(chars,"P0"))
-        index = P0;
-    else if(!strcmp(chars,"MF"))
-        index = MF;
-    else if(!strcmp(chars,"RR"))
-        index = RR;
-    else if(!strcmp(chars,"SS"))
-        index = SS;
-    else if(!strcmp(chars,"LR"))
-        index = LR;
-    else if(!strcmp(chars,"LG"))
-    	index = LG;
-    else if(!strcmp(chars,"GO"))
-        index = GO;
-    else if(!strcmp(chars,"R0"))
-        index = R0;
-    else if(!strcmp(chars,"R1"))
-        index = R1;
-    else if(!strcmp(chars,"TD"))
-        index = TD;
-    else if(!strcmp(chars,"DS"))
-        index = DS;
-    else if(!strcmp(chars,"ES"))
-        index = ES;
-    else if(!strcmp(chars,"DC"))
-        index = DC;
-    else if(!strcmp(chars,"BL"))
-        index = BL;
-    else
-        index = ER;
+	char index = 0;
+	if(!strcmp(chars,"HH"))
+		index = HH;
+	else if(!strcmp(chars,"MS"))
+		index = MS;
+	else if(!strcmp(chars,"P0"))
+		index = P0;
+	else if(!strcmp(chars,"MF"))
+		index = MF;
+	else if(!strcmp(chars,"RR"))
+		index = RR;
+	else if(!strcmp(chars,"SS"))
+		index = SS;
+	else if(!strcmp(chars,"LR"))
+		index = LR;
+	else if(!strcmp(chars,"LG"))
+		index = LG;
+	else if(!strcmp(chars,"GO"))
+		index = GO;
+	else if(!strcmp(chars,"R0"))
+		index = R0;
+	else if(!strcmp(chars,"R1"))
+		index = R1;
+	else if(!strcmp(chars,"TD"))
+		index = TD;
+	else if(!strcmp(chars,"DS"))
+		index = DS;
+	else if(!strcmp(chars,"ES"))
+		index = ES;
+	else if(!strcmp(chars,"DC"))
+		index = DC;
+	else if(!strcmp(chars,"BL"))
+		index = BL;
+	else
+		index = ER;
 
-    UARTprintf("\n");
-    UARTprintf(LookupTable[index]);
-    UARTCharPut(UART1_BASE, 0x20);
-    UARTprintf(LookupTable[index + 1]);
-    UARTprintf(" \r\n");
-    return index;
+	UARTprintf("\n");
+	UARTprintf(LookupTable[index]);
+	UARTCharPut(UART1_BASE, 0x20);
+	UARTprintf(LookupTable[index + 1]);
+	UARTprintf(" \r\n");
+	return index;
 }
 
 void getFunction(char function){
-    switch (function) {
-    case R1:
-        rightDistance();
-        frontDistance();
-        break;
-    case MF:
-    	move_forward();
-        break;
-    case RR:
-    	move_cw();
-        break;
-    case LR:
-    	move_ccw();
-        break;
-    case ES:
-        PWMStop();
-        break;
-    case SS:
-    	PWMUserSetSpeed();
-        break;
-    case MS:
-        PWMStart();
-        break;
-    case GO:
-        PIDStart();
-        break;
-    case BL:
-        break;
-    default:
-        UARTprintf("Error \n");
-        break;
-    }
+	switch (function) {
+	case R1:
+		rightDistance();
+		frontDistance();
+		break;
+	case MF:
+		move_forward();
+		break;
+	case RR:
+		move_cw();
+		break;
+	case LR:
+		move_ccw();
+		break;
+	case ES:
+		PWMStop();
+		break;
+	case SS:
+		PWMUserSetSpeed();
+		break;
+	case MS:
+		PWMStart();
+		break;
+	case GO:
+		PIDStart();
+		break;
+	case BL:
+		break;
+	default:
+		UARTprintf("Error \n");
+		break;
+	}
 }
 
 
 void UARTIntHandler(void)
 {
-    char characters[2] = " ";
-    char function = 0;
-    uint32_t ui32Status;
-    //get interrupt status
-    ui32Status = UARTIntStatus(UART1_BASE, true);
-    //clear the asserted interrupts
-    UARTIntClear(UART1_BASE, ui32Status);
-    UARTgets(characters, 3);
-    function = responseGet(characters);
-    getFunction(function);
-    UARTprintf("Enter Command: ");
+	char characters[2] = " ";
+	char function = 0;
+	uint32_t ui32Status;
+	//get interrupt status
+	ui32Status = UARTIntStatus(UART1_BASE, true);
+	//clear the asserted interrupts
+	UARTIntClear(UART1_BASE, ui32Status);
+	UARTgets(characters, 3);
+	function = responseGet(characters);
+	getFunction(function);
+	UARTprintf("Enter Command: ");
 }
 /********************************************************************************************************
  *                                     Interface Initialization Functions                               *
  ********************************************************************************************************/
 
 void UARTInit(void){
-    // UART Module 1
-    // Port PB0 RX
-    // Port PB1 TX
+	// UART Module 1
+	// Port PB0 RX
+	// Port PB1 TX
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 	// configuration of GPIO pins and types
 	GPIOPinConfigure(GPIO_PB0_U1RX);
-    GPIOPinConfigure(GPIO_PB1_U1TX);
+	GPIOPinConfigure(GPIO_PB1_U1TX);
 	GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 	// set clock and standard I/O
 	UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
@@ -445,10 +462,10 @@ void UARTInit(void){
 }
 
 void ADCInit(void) {
-    //  Port PE3 AIN-0 - ADC_CTL_CH0 - RIGHT DISTANCE SENSOR
-    //  Port PE2 AIN-1 - ADC_CTL_CH1 - FRONT DISTANCE SENSOR
-    //  Port PE5 AIN-8 - ADC_CTL_CH8 - RIGHT LIGHT SENSOR
-    //  Port PE4 AIN-9 - ADC_CTL_CH9 - LEFT LIGHT SENSOR
+	//  Port PE3 AIN-0 - ADC_CTL_CH0 - RIGHT DISTANCE SENSOR
+	//  Port PE2 AIN-1 - ADC_CTL_CH1 - FRONT DISTANCE SENSOR
+	//  Port PE5 AIN-8 - ADC_CTL_CH8 - RIGHT LIGHT SENSOR
+	//  Port PE4 AIN-9 - ADC_CTL_CH9 - LEFT LIGHT SENSOR
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
@@ -468,13 +485,13 @@ void ADCInit(void) {
 
 void PWMInit(void)
 {
-    //  Port PE1 - MODE = 1
-    //  Port PD3 - PHASE PIN - RIGHT MOTOR  // changed to PD7
-    //  Port PD1 - PWM PIN   - RIGHT MOTOR
-    //  Port PD2 - PHASE PIN - LEFT MOTOR
-    //  Port PD0 - PWM PIN   - LEFT MOTOR
-    // FORWARD 0 BITMASKED  // LOGIC SWITCHED - > FORWARD 1 BITMASKED
-    // REVERSE 1 BITMASKED  // LOGIC SWITCHED - > REVERSE 0 BITMASKED
+	//  Port PE1 - MODE = 1
+	//  Port PD3 - PHASE PIN - RIGHT MOTOR  // changed to PD7
+	//  Port PD1 - PWM PIN   - RIGHT MOTOR
+	//  Port PD2 - PHASE PIN - LEFT MOTOR
+	//  Port PD0 - PWM PIN   - LEFT MOTOR
+	// FORWARD 0 BITMASKED  // LOGIC SWITCHED - > FORWARD 1 BITMASKED
+	// REVERSE 1 BITMASKED  // LOGIC SWITCHED - > REVERSE 0 BITMASKED
 
 	SysCtlPWMClockSet(SYSCTL_PWMDIV_64);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
@@ -484,9 +501,9 @@ void PWMInit(void)
 	// hardware unlock for gpio pd7
 	HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
 	HWREG(GPIO_PORTD_BASE + GPIO_O_CR) |= 0x80;
-    HWREG(GPIO_PORTD_BASE + GPIO_O_AFSEL) &= ~0x80;
-    HWREG(GPIO_PORTD_BASE + GPIO_O_DEN) |= 0x80;
-    HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = 0;
+	HWREG(GPIO_PORTD_BASE + GPIO_O_AFSEL) &= ~0x80;
+	HWREG(GPIO_PORTD_BASE + GPIO_O_DEN) |= 0x80;
+	HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = 0;
 
 	GPIOPinTypePWM(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 	GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE,GPIO_PIN_2 | GPIO_PIN_7);
